e755cd314e8facbd1f320e14f33f63dd9603122c
Indra Maulana <indramaulana4455@gmail.com>
2025-11-17 10:47:06 +0700

    functions

---
+++ app.js
@@ -0,0 +1,19 @@
+const namadepan = "Indra";
+const namaBelakang = "Maulana"; 
+
+const fullname = (namadepan, namaBelakang) => {
+    
+    return namadepan + namaBelakang;
+};
+    
+const getNama = () => {
+    
+    return result ;
+};
+
+
+const result = fullname ("Indra" , "Maulana");
+
+ambilNama = getNama();
+
+console.log(ambilNama);
